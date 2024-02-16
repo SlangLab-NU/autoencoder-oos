23343a2ffce045a03cb3dc1f74ff5989f1dcc8ee
Tianyi Zhang <admin@ztybigcat.me>
2024-02-15 19:50:29 -0800

    remove unnecessary code

--- improved_ce_fine-convert.py
+++ improved_ce_fine-convert.py
@@ -3,7 +3,7 @@
 
 # # Load Data
 
-# In[26]:
+# In[ ]:
 
 
 import json
@@ -18,19 +18,9 @@ from scipy.linalg import inv
 from torch.optim import Adam
 
 
-# In[27]:
-
-
-# seed_value=10912
-# random.seed(seed_value)
-# np.random.seed(seed_value)
-# torch.manual_seed(seed_value)
-# torch.cuda.manual_seed_all(seed_value)
-
-
 # # Load CLINC150
 
-# In[28]:
+# In[ ]:
 
 
 # Load the dataset
@@ -63,7 +53,7 @@ model_name = "improved_ce_model_bert_CLINC150.pth"
 
 # # Load SLURP
 
-# In[4]:
+# In[ ]:
 
 
 # def load_data(file_path):
@@ -115,169 +105,9 @@ model_name = "improved_ce_model_bert_CLINC150.pth"
 # model_name = "improved_ce_model_bert_SLURP.pth"
 
 
-# # Load Banking77
-
-# In[5]:
-
-
-# # Define the IntentExample class and load_intent_examples function as provided
-# class IntentExample:
-#     def __init__(self, text, label, do_lower_case):
-#         self.original_text = text
-#         self.text = text
-#         self.label = label
-#         if do_lower_case:
-#             self.text = self.text.lower()
-
-# def load_intent_examples(file_path, do_lower_case=True):
-#     examples = []
-#     with open(f'{file_path}/seq.in', 'r', encoding="utf-8") as f_text, open(f'{file_path}/label', 'r', encoding="utf-8") as f_label:
-#         for text, label in zip(f_text, f_label):
-#             e = IntentExample(text.strip(), label.strip(), do_lower_case)
-#             examples.append(e)
-#     return examples
-
-# # Define paths to the dataset directories
-# base_dir = 'Few-Shot-Intent-Detection/Datasets/BANKING77-OOS'
-# paths = {
-#     'train': f'{base_dir}/train',
-#     'valid': f'{base_dir}/valid',
-#     'test': f'{base_dir}/test',
-#     'oos_val': f'{base_dir}/ood-oos/valid',
-#     'oos_test': f'{base_dir}/ood-oos/test'
-# }
-# datasets = {key: load_intent_examples(path) for key, path in paths.items()}
-
-# # Extract sentences and labels from the loaded datasets
-# train_sentences = [e.text for e in datasets['train']]
-# train_labels = [e.label for e in datasets['train']]
-
-# val_sentences = [e.text for e in datasets['valid']]
-# val_labels = [e.label for e in datasets['valid']]
-
-# test_sentences = [e.text for e in datasets['test']]
-# test_labels = [e.label for e in datasets['test']]
-
-# oos_val_sentences = [e.text for e in datasets['oos_val']]
-# oos_test_sentences = [e.text for e in datasets['oos_test']]
-# model_name = "improved_ce_model_bert_BANKING77.pth"
-
-
-# # SNIPS
-
-# In[6]:
-
-
-# # Define the IntentExample class and load_intent_examples function as provided
-# class IntentExample:
-#     def __init__(self, text, label, do_lower_case):
-#         self.original_text = text
-#         self.text = text
-#         self.label = label
-#         if do_lower_case:
-#             self.text = self.text.lower()
-
-# def load_intent_examples(file_path, do_lower_case=True):
-#     examples = []
-#     with open(f'{file_path}/seq.in', 'r', encoding="utf-8") as f_text, open(f'{file_path}/label', 'r', encoding="utf-8") as f_label:
-#         for text, label in zip(f_text, f_label):
-#             e = IntentExample(text.strip(), label.strip(), do_lower_case)
-#             examples.append(e)
-#     return examples
-
-# # Define paths to the dataset directories
-# base_dir = 'Few-Shot-Intent-Detection/Datasets/SNIPS'
-# paths = {
-#     'train': f'{base_dir}/train',
-#     'valid': f'{base_dir}/valid',
-#     'test': f'{base_dir}/test'
-# }
-# datasets = {key: load_intent_examples(path) for key, path in paths.items()}
-
-# # Extract sentences and labels from the loaded datasets
-# train_sentences = [e.text for e in datasets['train']]
-# train_labels = [e.label for e in datasets['train']]
-
-# val_sentences = [e.text for e in datasets['valid']]
-# val_labels = [e.label for e in datasets['valid']]
-
-# test_sentences = [e.text for e in datasets['test']]
-# test_labels = [e.label for e in datasets['test']]
-# unique_scenarios = set(train_labels)
-# # oos_scenario = random.choice(list(unique_scenarios))
-# oos_scenario = 'AddToPlaylist'
-# # Separate out of scope data
-# oos_train_data = [(s, l) for s, l in zip(train_sentences, train_labels) if l == oos_scenario]
-# oos_val_data = [(s, l) for s, l in zip(val_sentences, val_labels) if l == oos_scenario]
-# oos_test_data = [(s, l) for s, l in zip(test_sentences, test_labels) if l == oos_scenario]
-
-# # Remove out of scope data from original sets
-# train_data = [(s, l) for s, l in zip(train_sentences, train_labels) if l != oos_scenario]
-# val_data = [(s, l) for s, l in zip(val_sentences, val_labels) if l != oos_scenario]
-# test_data = [(s, l) for s, l in zip(test_sentences, test_labels) if l != oos_scenario]
-# # Extract sentences and labels
-# train_sentences = [item[0] for item in train_data]
-# train_labels = [item[1] for item in train_data]
-
-# val_sentences = [item[0] for item in val_data]
-# val_labels = [item[1] for item in val_data]
-
-# test_sentences = [item[0] for item in test_data]
-# test_labels = [item[1] for item in test_data]
-
-# oos_train_sentences = [item[0] for item in oos_train_data]
-# oos_val_sentences = [item[0] for item in oos_val_data]
-# oos_test_sentences = [item[0] for item in oos_test_data]
-# oos_scenario
-
-# model_name = "improved_ce_model_bert_SNIP.pth"
-
-
-# # ROSTD
-
-# In[8]:
-
-
-# from datasets import load_dataset
-
-# dataset = load_dataset("cmaldona/Generalization-MultiClass-CLINC150-ROSTD", "rostd+")
-
-# train_sentences = []
-# train_labels = []
-# val_sentences = []
-# val_labels = []
-# test_sentences = []
-# test_labels = []
-# oos_test_sentences = []
-
-# # Extract training data
-# for example in dataset['train']:
-#     train_sentences.append(example['data'].lower())
-#     train_labels.append(example['labels'])
-
-# # Extract validation data
-# for example in dataset['validation']:
-#     val_sentences.append(example['data'].lower())
-#     val_labels.append(example['labels'])
-
-# # Extract test data and separate ID from OOS
-# for example in dataset['test']:
-#     if example['generalisation'] == 'ID':
-#         test_sentences.append(example['data'].lower())
-#         test_labels.append(example['labels'])
-#     elif example['generalisation'] == 'near-OOD' or example['generalisation'] == 'far-OOD':# OOS
-#         try:
-#             oos_test_sentences.append(example['data'].lower())
-#         except:
-#             continue
-            
-
-# model_name = "improved_ce_model_bert_ROSTD.pth"
-
-
 # # Encode Labels
 
-# In[29]:
+# In[ ]:
 
 
 label_encoder = LabelEncoder()
@@ -288,7 +118,7 @@ encoded_val_labels = label_encoder.fit_transform(val_labels)
 
 # # Tokenize our sentences and create Dataloaders
 
-# In[30]:
+# In[ ]:
 
 
 pretrained_model_name = "bert-base-uncased"
@@ -310,7 +140,7 @@ class TextDataset(Dataset):
 
 
 
-# In[31]:
+# In[ ]:
 
 
 tokenized_lengths = [len(tokenizer.encode(sentence, add_special_tokens=True)) for sentence in train_sentences]
@@ -323,7 +153,7 @@ val_dataset = TextDataset(val_sentences, encoded_val_labels, tokenizer, max_leng
 
 # # Define functions to encode our sentences
 
-# In[32]:
+# In[ ]:
 
 
 transformer_model.eval()
@@ -355,7 +185,7 @@ def encode_sentences(model, sentences, tokenizer=tokenizer, batch_size=256):
 
 # # Define our model
 
-# In[33]:
+# In[ ]:
 
 
 import torch.nn as nn
@@ -378,15 +208,10 @@ class TextClassifier(nn.Module):
 
 # # Initiallize everything else we needed
 
-# In[34]:
+# In[ ]:
 
 
 unique_intents = list(set(train_labels)) 
-transformer_model = AutoModel.from_pretrained(pretrained_model_name)
-transformer_model.to(device)
-model = TextClassifier(transformer_model, len(unique_intents))
-model.to(device)
-optimizer = Adam(model.parameters(), lr=5.2849051227677665e-06)
 training_losses = []
 validation_losses = []
 batch_size= 16
@@ -395,18 +220,7 @@ val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
 loss_function = nn.CrossEntropyLoss()
 
 
-# In[35]:
-
-
-# def euclidean_distance_loss(embeddings):
-#     n, k = embeddings.size()
-#     mean_embeddings = embeddings.mean(dim=0)
-#     distances = embeddings - mean_embeddings
-#     loss = (distances ** 2).sum(dim=1).mean() / k
-#     return loss
-
-
-# In[36]:
+# In[ ]:
 
 
 def euclidean_distance_loss(embeddings):
@@ -430,200 +244,13 @@ def euclidean_distance_loss(embeddings):
     return loss
 
 
-# In[37]:
-
-
-ed_loss_importance = 0.005302264337910589
-num_epochs = 25
-
-
-# # Training Loop!!!
-
-# In[38]:
-
-
-best_val_loss = float('Inf')
-for epoch in range(num_epochs):
-    # Training Phase
-    model.train()  # Set the model to training mode
-    total_train_loss = 0
-    for batch in train_dataloader:
-        input_ids, attention_mask, labels = batch['input_ids'].to(device), batch['attention_mask'].to(device), batch['labels'].to(device)
-        optimizer.zero_grad()  # Zero the gradients
-        predictions, embeddings = model(input_ids, attention_mask)  # Forward pass
-        ce_loss = loss_function(predictions, labels)  # Cross-Entropy loss
-        ed_loss = euclidean_distance_loss(embeddings)  # Euclidean distance loss
-        total_loss = ce_loss + ed_loss_importance * ed_loss  # Combine the losses
-        
-        total_loss.backward()  # Backward pass
-        optimizer.step()  # Update weights
-
-        total_train_loss += total_loss.item()
-    
-    avg_train_loss = total_train_loss / len(train_dataloader)
-    training_losses.append(avg_train_loss) 
-
-    # Validation Phase
-    model.eval()  # Set the model to evaluation mode
-    total_val_loss = 0
-    with torch.no_grad():  # Disable gradient calculations
-        for batch in val_dataloader:
-            input_ids, attention_mask, labels = batch['input_ids'].to(device), batch['attention_mask'].to(device), batch['labels'].to(device)
-            predictions, embeddings = model(input_ids, attention_mask)  # Forward pass
-            ce_loss = loss_function(predictions, labels)  # Cross-Entropy loss
-            ed_loss = euclidean_distance_loss(embeddings)  # Euclidean distance loss
-            total_loss = ce_loss + ed_loss_importance * ed_loss  # Combine the losses
-            total_val_loss += total_loss.item()
-    avg_val_loss = total_val_loss / len(val_dataloader)
-    if avg_val_loss < best_val_loss:
-        best_val_loss = avg_val_loss
-        # Save the model
-        torch.save(model, model_name)
-        print(f"Epoch {epoch+1}/{num_epochs}: Lower validation loss found. Model saved.")
-    validation_losses.append(avg_val_loss)
-    print(f"Epoch {epoch+1}/{num_epochs}, Training Loss: {avg_train_loss:.5e}, Validation Loss: {avg_val_loss:.5e}")
-
-
-# # Calculate means and covariance matrix
-
-# In[42]:
-
-
-fine_model = torch.load(model_name)
-fine_model.eval()  # Put the model in evaluation mode
-fine_model = fine_model.to(device)
-
-
-# In[43]:
-
-
-# get trasformer part of the model
-fine_model = fine_model.transformer
-
-
-# In[44]:
-
-
-train_embeddings = encode_sentences(fine_model, train_sentences)
-val_embeddings = encode_sentences(fine_model, val_sentences)
-test_embeddings = encode_sentences(fine_model, test_sentences)
-oos_val_embeddings = encode_sentences(fine_model, oos_val_sentences)
-oos_test_embeddings = encode_sentences(fine_model, oos_test_sentences)
-
-
-# In[45]:
-
-
-intent_means = {}
-for encoded_label in np.unique(encoded_train_labels):
-    # Find indices where the encoded label matches
-    indices = np.where(encoded_train_labels == encoded_label)[0]
-    
-    # Calculate the mean embedding for the current intent
-    intent_embeddings = train_embeddings[indices]
-    intent_mean = np.mean(intent_embeddings, axis=0)
-    
-    # Use the encoded label as the dictionary key
-    intent_means[encoded_label] = intent_mean
-
-
-# In[46]:
-
-
-covariance = np.cov(train_embeddings, rowvar=False)
-cov_inverse = inv(covariance)
-
-
-# In[47]:
-
-
-from sklearn.metrics import roc_auc_score, average_precision_score, roc_curve
-from scipy.spatial import distance
-from sklearn.metrics import average_precision_score
-
-
-# In[48]:
-
-
-def min_mahalanobis_for_sample(sample, intent_means, cov_inverse):
-    distances = [distance.mahalanobis(sample, mean, cov_inverse) for mean in intent_means.values()]
-    return min(distances)
-
-
-# In[49]:
-
-
-# Compute minimum Mahalanobis distances for samples in test_embeddings and oos_test_embeddings
-val_scores = [min_mahalanobis_for_sample(sample, intent_means, cov_inverse) for sample in val_embeddings]
-oos_val_scores = [min_mahalanobis_for_sample(sample, intent_means, cov_inverse) for sample in oos_val_embeddings]
-
-# True binary labels: 0 for in-domain and 1 for OOD
-y_true = [0] * len(val_scores) + [1] * len(oos_val_scores)
-
-# Combine the scores
-y_scores = val_scores + oos_val_scores
-
-# Compute AUPR
-aupr = average_precision_score(y_true, y_scores)
-aupr
-
-
-# In[50]:
-
-
-# Compute minimum Mahalanobis distances for samples in test_embeddings and oos_test_embeddings
-test_scores = [min_mahalanobis_for_sample(sample, intent_means, cov_inverse) for sample in test_embeddings]
-oos_test_scores = [min_mahalanobis_for_sample(sample, intent_means, cov_inverse) for sample in oos_test_embeddings]
-
-# True binary labels: 0 for in-domain and 1 for OOD
-y_true = [0] * len(test_scores) + [1] * len(oos_test_scores)
-
-# Combine the scores
-y_scores = test_scores + oos_test_scores
-
-# Compute AUPR
-aupr = average_precision_score(y_true, y_scores)
-aupr
-
-
-# In[27]:
-
-
-auroc = roc_auc_score(y_true, y_scores)
-auroc
-
-
-# In[237]:
-
-
-# import matplotlib.pyplot as plt
-
-# # Set up the figure and axes
-# plt.figure(figsize=(10, 6))
-
-# # Plot the histograms
-# plt.hist(test_scores, bins=50, alpha=0.5, label='In-domain')
-# plt.hist(oos_test_scores, bins=50, alpha=0.5, label='Out-of-domain')
-
-# # Add legend, title, and labels
-# plt.legend(loc='upper right')
-# plt.title('Distribution of Minimum Mahalanobis Distances')
-# plt.xlabel('Mahalanobis Distance')
-# plt.ylabel('Number of Samples')
-# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-# plt.tight_layout()
-
-# # Show the plot
-# plt.show()
-
-
-# In[52]:
+# In[ ]:
 
 
 from torch.utils.tensorboard import SummaryWriter
 
 
-# In[55]:
+# In[ ]:
 
 
 import optuna
@@ -747,22 +374,6 @@ study.optimize(objective, n_trials=1000)  # n_trials is the number of iterations
 best_params = study.best_params
 print("Best parameters:", best_params)
 
-
-
-# In[2]:
-
-
-import optuna
-study = optuna.load_study(study_name='improved_ce_loss_CLINC150', storage='sqlite:///desperate.db')
-
-
-# In[3]:
-
-
-sorted_trials = sorted(
-    study.trials, 
-    key=lambda trial: min(trial.user_attrs.get('validation_losses', [float('inf')]))
-)
 
 
 # In[ ]:
